f44688f86c6f653a59921edfe3576d702a4bebd7
jahidhsanto <75691112+jahidhsanto@users.noreply.github.com>
2021-07-09 02:17:42 +0600

    2nd Lab report

---
+++ src/Report_2_1/odd.java
@@ -0,0 +1,19 @@
+package Report_2_1;
+
+public class odd {
+    int n;
+
+    // Apply this section for only Constructor
+    odd(int num) {
+        n = num;
+    }
+
+    // By this method check the number is odd or even
+    int check_odd() {
+        if (n % 2 == 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
